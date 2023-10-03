a92ede78a29ffd82af6f1194419dfd9cf3fcd9d3
kavishbhawar <kavish.bhawar@gmail.com>
2023-10-03 11:42:08 +0530

    first commit

---
+++ src/main/java/com/dockertest/sampleapp/SampleappApplication.java
@@ -0,0 +1,22 @@
+package com.dockertest.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleappApplication {
+	
+	@GetMapping("/test")
+	public String getMessage() {
+		
+		return "testing docker app1";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleappApplication.class, args);
+	}
+
+}
